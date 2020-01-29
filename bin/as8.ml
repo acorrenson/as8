d63cdc0b7a4475d48d9ce0325270134c518b37b4
Arthur Correnson <arthur.correnson@gmail.com>
2020-01-29 10:06:51 +0100

    Update CLI with sub commands

--- bin/as8.ml
+++ bin/as8.ml
@@ -5,25 +5,24 @@ let input =
   let docv = "source.as8" in
   Arg.(required & pos 0 (some file) None & info [] ~doc ~docv)
 
-let f =
-  let doc = "a flag" in 
-  let docv = "a flag" in
-  Arg.(value & flag & info ["f"] ~doc ~docv)
-
-let run file f =
-  print_endline file;
-  if f then print_endline "f"
+let build file =
+  print_endline file
 
 let cmd_run =
-  let doc = "Assembler for CHIP8 assembly language" in
+  let doc = "build a CHIP8 assembly file" in
   let man = [
     `S Manpage.s_authors;
     `P "Arthur Correnson <arthur.correnson@univ-tlse3.fr>";
     `S Manpage.s_bugs;
     `P "If you find a bug, please send an email to <arthur.correnson@univ-tlse3.fr>";
   ] in
-  Term.(const run $ input $ f),
-  Term.info "as8" ~version:"v0.1" ~exits:Term.default_exits ~doc ~man
+  Term.(const build $ input),
+  Term.info "build" ~version:"v0.1" ~exits:Term.default_exits ~doc ~man
+
+let cmd =
+  let err = Term.(const (fun () -> `Error (true, "no command provided")) $ const ()) in
+  Term.(ret err),
+  Term.info "as8" ~version:"v0.1" ~exits:Term.default_exits
 
 let _ =
-  Term.(exit @@ eval cmd_run)
+  Term.(exit @@ eval_choice cmd [cmd_run])
